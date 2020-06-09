dfc3de1d4f40b0eca5688f0005bc4e67cc904001
Malverick <jamesfirth95@yahoo.co.uk>
2020-06-09 10:52:22 +0100

    Tried a few things, didn't work

--- src/main/java/com/crd/example/ExampleApplication.java
+++ src/main/java/com/crd/example/ExampleApplication.java
@@ -1,14 +1,14 @@
 package com.crd.example;
 
 import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+//@EnableAutoConfiguration
 @ComponentScan
-//@ComponentScan(basePackages = "com.crd.example.endpoint")
-//@ComponentScan(basePackages = "com.crd.example.routes")
 public class ExampleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/crd/example/model/Movie.java
+++ src/main/java/com/crd/example/model/Movie.java
@@ -1,17 +1,22 @@
 package com.crd.example.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+//import org.springframework.data.relational.core.mapping.Table;
+
 import lombok.Data;
 
 @Data
+//@Table(value="movies")
 public class Movie {
 	
-	@SuppressWarnings("unused")
+	@Id
 	private int id;
 	
-	@SuppressWarnings("unused")
+	@Column
 	private String title;
 	
-	@SuppressWarnings("unused")
+	@Column
 	private String about;
 
 }
