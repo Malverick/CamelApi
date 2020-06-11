59d5193da764eb58a10c7d2595618c777463306d
Malverick <jamesfirth95@yahoo.co.uk>
2020-06-11 12:02:22 +0100

    Added component tags to PostRouting Class

--- src/main/java/com/crd/example/model/Movie.java
+++ src/main/java/com/crd/example/model/Movie.java
@@ -7,6 +7,7 @@ import lombok.Data;
 
 @Data
 public class Movie {
+	
 	@SuppressWarnings("all")
 	public Movie() {
 		this.title = title;
--- src/main/java/com/crd/example/routes/PostRouting.java
+++ src/main/java/com/crd/example/routes/PostRouting.java
@@ -5,7 +5,12 @@ import javax.sql.DataSource;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
 public class PostRouting extends RouteBuilder {
 	
 	@Autowired
